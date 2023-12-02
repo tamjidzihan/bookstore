f778dbb352706fab26e6ad69120c123affd409ca
tamjidzihan <tamjidzihan@gmail.com>
2023-12-02 15:17:33 +0600

    adding front end

---
+++ frontend/static/js/app.js
@@ -0,0 +1,9 @@
+const slideImage = document.querySelectorAll(".slide-image");
+const slidesContainer = document.querySelector(".slides-container");
+const nextBtn = document.querySelector(".next-btn");
+const pervBtn = document.querySelector(".perv-btn");
+const navigationsDots = document.querySelector(".navigation-dots");
+
+function init() {
+
+}
